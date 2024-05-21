029d70ba2da666fe883573cb0a072580b5340233
maesedev <santiagogamer51@gmail.com>
2024-05-21 14:09:34 -0500

    Movimos los modulos y ya funciona el subir al S3 las canciones

---
+++ modules/s3_manager.py
@@ -0,0 +1,40 @@
+import boto3
+def upload_file_to_s3(file_path, bucket_name, object_name):
+    """
+    Sube un archivo al bucket de S3 especificado.
+
+    Args:
+        file_path (str): Ruta local del archivo a subir.
+        bucket_name (str): Nombre del bucket de S3.
+        object_name (str): Nombre que se le dará al objeto en S3.
+
+    Returns:
+        bool: True si el archivo se subió correctamente, False en caso contrario.
+    """
+
+    # Inicializar el cliente de S3
+    s3_client = boto3.client('s3')
+
+    try:
+        # Subir el archivo al bucket de S3
+        s3_client.upload_file(file_path, bucket_name, object_name)
+    except Exception as e:
+        print(f"Error al subir el archivo a S3: {e}")
+        return False
+    else:
+        print(f"Archivo subido exitosamente a S3: {object_name}")
+        return True
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    # Ruta local del archivo de audio
+    audio_file_path = "song/Swans - Will We Survive.mp3"
+
+    # Nombre del bucket de S3
+    s3_bucket_name = "checho-bucket-9089"
+
+    # Nombre que se le dará al objeto en S3
+    s3_object_name = "Test_audio.mp3"
+
+    # Subir el archivo al bucket de S3
+    upload_file_to_s3(audio_file_path, s3_bucket_name, s3_object_name)
